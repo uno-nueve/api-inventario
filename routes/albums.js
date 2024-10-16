0a92e126448acb974d8ad7cfe1ea93f3b018ccbc
Luciano Montilla <unonueve95@gmail.com>
2024-10-16 17:35:52 -0300

    Create business logic endpoints

--- routes/albums.js
+++ routes/albums.js
@@ -71,4 +71,76 @@ router.delete("/albums/:id", async (req, res) => {
     }
 });
 
+/* ************************************************************************************ */
+
+//Obtener albums según filtros de búsqueda
+router.get("/albums/catalog/search", async (req, res) => {
+    const { artista, genero, estado } = req.query;
+
+    try {
+        const query = {};
+        if (artista) query.artista = artista;
+        if (genero) query.genero = genero;
+        if (estado) query.estado = estado;
+
+        const albums = await Album.find(query);
+
+        if (!albums.length) {
+            return res.status(404).send({
+                message: "No se encontraron albums con los criterios proporcionados",
+            });
+        }
+
+        res.status(200).send(albums);
+    } catch (error) {
+        res.status(500).send({ message: "Error al buscar albums", error });
+    }
+});
+
+//Actualizar stock de un album al comprar
+router.put("/albums/:id/buy", async (req, res) => {
+    try {
+        const album = await Album.findById(req.params.id);
+
+        if (!album) {
+            return res.status(404).send({ message: "Album no encontrado" });
+        }
+
+        if (album.stock === 1) {
+            album.estado = "Agotado";
+        }
+
+        album.stock = album.stock - 1;
+        album.fechaCompra = new Date();
+        await album.save();
+
+        res.status(200).send(album);
+    } catch (error) {
+        res.status(400).send({ message: "Error al actualizar el stock del album", error });
+    }
+});
+
+//Actualizar el stock de un album al devolverlo
+router.put("/albums/:id/return", async (req, res) => {
+    try {
+        const album = await Album.findById(req.params.id);
+
+        if (!album) {
+            return res.status(404).send({ message: "Album no encontrado" });
+        }
+
+        if (album.stock === 0) {
+            album.estado = "Disponible";
+        }
+
+        album.stock = album.stock + 1;
+        album.fechaDevolución = new Date();
+        await album.save();
+
+        res.status(200).send(album);
+    } catch (error) {
+        res.status(400).send({ message: "Error al devolver album", error });
+    }
+});
+
 module.exports = router;
