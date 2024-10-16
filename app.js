1fe0dc941844137abc1b5ec8e7a87d9366e6f8dd
Luciano Montilla <unonueve95@gmail.com>
2024-10-16 15:21:29 -0300

    Create CRUD endpoints

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const dbConnect = require("./config/db");
+const albumsRouter = require("./routes/albums");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 const HOST = process.env.HOST || "localhost";
 
 app.use(express.json());
+app.use(albumsRouter);
 
 app.get("/", (req, res) => {
     res.send("Hello world!");
